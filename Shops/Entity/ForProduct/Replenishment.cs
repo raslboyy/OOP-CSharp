9fd8a446f97ede5aedd602b0b2023cd4a68ae2b4
Ruslan <ruslankhakimov67@gmail.com>
2021-10-29 15:34:04 +0300

    erase lab-1

--- Shops/Entity/ForProduct/Replenishment.cs
+++
@@ -1,6 +0,0 @@
-namespace Shops.Entity.ForProduct
-{
-    public class Replenishment : AProductList
-    {
-    }
-}
--- Shops/Entity/ForProduct/Storage.cs
+++
@@ -1,7 +0,0 @@
-namespace Shops.Entity.ForProduct
-{
-    internal class Storage : AProductList
-    {
-        public uint GetPrice(ProductName name) => Get(name).Price;
-    }
-}
