e7f2fdc39e7c46f1a4940118de2a15114e35810c
Ruslan <ruslankhakimov67@gmail.com>
2021-10-30 17:14:14 +0300

    refactor: clean out Program.cs

--- Shops/Program.cs
+++ Shops/Program.cs
@@ -8,24 +8,6 @@ namespace Shops
     {
         private static void Main()
         {
-            var p = new ProductCountPrice(new Product("fsdf"), 0, 4);
-            Foo(p);
-            Console.WriteLine(p.Count);
         }
-
-        private static void Foo(ProductCountPrice p)
-        {
-            p = (ProductCountPrice)p.Clone();
-            p.Count = 3;
-        }
-
-        // private static CustomerName RegisterDefaultCustomer(Service service) =>
-        //     service.RegisterCustomer("Customer", 100);
-        //
-        // private static ShopName RegisterDefaultShop(Service service) =>
-        //     service.RegisterShop("Shop", CreateDefaultAddress());
-        //
-        // private static Address CreateDefaultAddress() => new Address("Country", "City", "Street", "Building");
-        // private static Service CreateDefaultService() => new Service();
     }
 }
