f332e046f3048a82ed7311c095dc1314ccbe4136
Ruslan <ruslankhakimov67@gmail.com>
2021-11-03 07:17:10 +0300

    initial lab-3

--- Backups/Program.cs
+++ Backups/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace Backups
+﻿using System;
+using Backups.Entities;
+using Backups.Entities.JobObjectModule;
+using Backups.Entities.RepositoryModule;
+using Backups.Entities.RestorePointModule;
+using Backups.Entities.StorageAlgorithmModule;
+
+namespace Backups
 {
     internal class Program
     {
         private static void Main()
         {
+            IBackupJob backupJob = new BackupJob("backup", new LocalRepository(), new SingleStorage());
+            backupJob.AddFile("test1");
+            backupJob.AddFile("test2");
+            backupJob.CreateRestorePoint();
         }
     }
 }
