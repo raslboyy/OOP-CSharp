65fb2d915bfd9a0bc5875798960d7de14b279465
raslboyy <ruslankhakimov67@gmail.com>
2021-11-20 01:47:24 +0300

    feat: add interface of account.

--- Banks/Entities/ClientModule/Client.cs
+++ Banks/Entities/ClientModule/Client.cs
@@ -1,3 +1,7 @@
+using System.Collections.Generic;
+using System.Linq;
+using Banks.Entities.AccountModule;
+
 namespace Banks.Entities.ClientModule
 {
     public class Client : IClient
@@ -7,5 +11,28 @@ namespace Banks.Entities.ClientModule
         public string Address { get; set; }
         public string Passport { get; set; }
         public bool IsGood { get; set; }
+        private List<AAccount> Accounts { get; } = new List<AAccount>();
+        public IAccount CreateDebitAccount(double value)
+        {
+            var account = new DebitAccount(value);
+            Accounts.Add(account);
+            return account;
+        }
+
+        public IAccount CreateDepositAccount(double value, int term)
+        {
+            var account = new DepositAccount(value, term);
+            Accounts.Add(account);
+            return account;
+        }
+
+        public IAccount CreateCreditAccount(double value)
+        {
+            var account = new CreditAccount(value);
+            Accounts.Add(account);
+            return account;
+        }
+
+        public IAccount GetAccount(int id) => Accounts.FirstOrDefault(account => account.Id == id);
     }
 }
--- Banks/Entities/ClientModule/IClient.cs
+++ Banks/Entities/ClientModule/IClient.cs
@@ -1,7 +1,13 @@
+using Banks.Entities.AccountModule;
+
 namespace Banks.Entities.ClientModule
 {
     public interface IClient
     {
         public bool IsGood { get; }
+        public IAccount CreateDebitAccount(double value);
+        public IAccount CreateDepositAccount(double value, int term);
+        public IAccount CreateCreditAccount(double value);
+        public IAccount GetAccount(int id);
     }
 }
