57137fc1576ccf28c66d6c056113d64711158f35
raslboyy <ruslankhakimov67@gmail.com>
2021-11-20 07:11:59 +0300

    test: add to transfer methods.

---
+++ Banks.Tests/AccountTransferTests.cs
@@ -0,0 +1,130 @@
+using System.Collections.Generic;
+using Banks.Entities.AccountModule;
+using Banks.Entities.BankModule;
+using Banks.Entities.BankModule.ConfigurationModule;
+using Banks.Entities.ClientModule;
+using Banks.Entities.ClientModule.ClientBuilderModule;
+using Banks.Tools;
+using NUnit.Framework;
+
+namespace Banks.Tests
+{
+    public class AccountTransferTests
+    {
+        [Test]
+        public void Transfer_DefaultTransfer_ReturnTrueChangedTheBalanceOfBothAccounts()
+        {
+            IAccount account1 = CreateDefaultFromAccount();
+            IAccount account2 = CreateDefaultToAccount();
+            double startValue = account1.Balance;
+            
+            TransferResult actual = account1.TransferTo(account2.Id, startValue);
+
+            Assert.AreEqual(true, actual.IsSuccessful);
+            Assert.AreEqual(0, account1.Balance);
+            Assert.AreEqual(startValue * 2, account2.Balance);
+        }
+
+        [Test]
+        public void Transfer_NotEnoughMoney_ReturnFalseNotChangedTheBalanceOfBothAccounts()
+        {
+            IAccount account1 = CreateDefaultFromAccount();
+            IAccount account2 = CreateDefaultToAccount();
+            double startValue = account1.Balance;
+            
+            TransferResult actual = account1.TransferTo(account2.Id, startValue + 1);
+
+            Assert.AreEqual(false, actual.IsSuccessful);
+            Assert.AreEqual(startValue, account1.Balance);
+            Assert.AreEqual(startValue, account2.Balance);
+        }
+
+        [Test]
+        public void Transfer_NotFoundToAccount_ReturnFalseNotChangeBalance()
+        {
+            IAccount account1 = CreateDefaultFromAccount();
+            double startValue = account1.Balance;
+            
+            TransferResult actual = account1.TransferTo(int.MaxValue, startValue);
+
+            Assert.AreEqual(false, actual.IsSuccessful);
+            Assert.AreEqual(startValue, account1.Balance);
+        }
+
+        [Test]
+        public void CancelTransfer_DefaultCancel_MoneyReturned()
+        {
+            IAccount account1 = CreateDefaultFromAccount();
+            IAccount account2 = CreateDefaultToAccount();
+            double startValue = account1.Balance;
+            
+            TransferResult transfer = account1.TransferTo(account2.Id, startValue / 2);
+            account1.TransferTo(account2.Id, startValue / 2);
+            account1.CancelTransfer(transfer.IdTransfer);
+
+            Assert.AreEqual(true, transfer.IsSuccessful);
+            Assert.AreEqual(startValue / 2, account1.Balance);
+            Assert.AreEqual(startValue * 2, account2.Balance);
+        }
+
+        [Test]
+        public void CancelTransfer_CancelTwice_ThrowTransferException()
+        {
+            IAccount account1 = CreateDefaultFromAccount();
+            IAccount account2 = CreateDefaultToAccount();
+            double startValue = account1.Balance;
+            
+            TransferResult transfer = account1.TransferTo(account2.Id, startValue / 2);
+            account1.TransferTo(account2.Id, startValue / 2);
+            account1.CancelTransfer(transfer.IdTransfer);
+            
+            Assert.Catch<TransferException>(() =>
+            {
+                account1.CancelTransfer(transfer.IdTransfer);
+            });
+        }
+        
+        [Test]
+        public void CancelTransfer_IncorrectId_ThrowTransferException()
+        {
+            IAccount account1 = CreateDefaultFromAccount();
+            
+            Assert.Catch<TransferException>(() =>
+            {
+                account1.CancelTransfer(int.MaxValue);
+            });
+        }
+
+        private static BankConfiguration CreateDefaultBankConfiguration()
+        {
+            var credit = new CreditCondition(15, -1000);
+            var debit = new DebitCondition(15);
+            var deposit = new DepositCondition(new List<ValuePercent>()
+            {
+                new ValuePercent(100, 2),
+                new ValuePercent(500, 4),
+                new ValuePercent(1000, 7)
+            });
+
+            return new BankConfiguration(credit, debit, deposit);
+        }
+
+        private static IAccount CreateDefaultFromAccount()
+        {
+            IBank bank = CentralBank.RegisterBank("Sber", CreateDefaultBankConfiguration());
+            var builder = new LastClientBuilder();
+            builder.SetFirstName("First").SetLastName("Last");
+            IClient client = bank.AddClient(builder);
+            return client.CreateDebitAccount(1000);
+        }
+        
+        private static IAccount CreateDefaultToAccount()
+        {
+            IBank bank = CentralBank.RegisterBank("Tink", CreateDefaultBankConfiguration());
+            var builder = new LastClientBuilder();
+            builder.SetFirstName("First").SetLastName("Last");
+            IClient client = bank.AddClient(builder);
+            return client.CreateDebitAccount(1000);
+        }
+    }
+}
