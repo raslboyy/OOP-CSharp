885f08466e20e4efbb580b08c9fef4450f0ca0d9
raslboyy <ruslankhakimov67@gmail.com>
2021-12-03 17:08:56 +0300

    feat: definition delete limit algorithm.

--- BackupsExtra/Entities/RestorePointModule/IForLimitAlgorithmModule/DeleteLimitAlgorithm.cs
+++ BackupsExtra/Entities/RestorePointModule/IForLimitAlgorithmModule/DeleteLimitAlgorithm.cs
@@ -8,7 +8,8 @@ namespace BackupsExtra.Entities.RestorePointModule.IForLimitAlgorithmModule
     {
         public IRestorePoint Execute(IStorageAlgorithmExtra storageAlgorithm, IRepositoryExtra repository, IRestorePoint point1, IRestorePoint point2)
         {
-            throw new System.NotImplementedException();
+            storageAlgorithm.DeleteRestorePoint(repository, point1);
+            return point2;
         }
     }
 }
