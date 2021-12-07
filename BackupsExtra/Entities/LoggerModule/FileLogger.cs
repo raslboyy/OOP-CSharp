386b23ab960cb808d1dc1b57646094cce40a1185
raslboyy <ruslankhakimov67@gmail.com>
2021-12-07 17:14:49 +0300

    fix: file name in file logger.

--- BackupsExtra/Entities/LoggerModule/FileLogger.cs
+++ BackupsExtra/Entities/LoggerModule/FileLogger.cs
@@ -6,15 +6,19 @@ namespace BackupsExtra.Entities.LoggerModule
     [DataContract]
     public class FileLogger : ILogger
     {
-        public FileLogger()
+        public FileLogger(string fileName)
         {
-            if (!File.Exists("backup_log"))
-                File.Create("backup_log");
+            FileName = fileName;
+            if (!File.Exists(FileName))
+                File.Create(FileName);
         }
+
+        [DataMember]
+        public string FileName { get; private set; }
 
         public void Log(string message)
         {
-            File.AppendAllText("backup_log", message + "\n");
+            File.AppendAllText(FileName, message + "\n");
         }
     }
 }
